5677943da28a8c9b857fe2c16ce95e0cafc4198d
GrushinVladimir <119972894+GrushinVladimir@users.noreply.github.com>
2024-09-30 11:36:13 +0300

    initial

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Button from '../Button/Button';
-import { useTelegram } from '../../hooks/useTelegram';
+import { useTelegram } from '../hooks/useTelegram';
 
 
 
 const Header = () => {
-    const {tg, user, onClose} = useTelegram();
+    const {user, onClose} = useTelegram();
 
     return(
         <div className={'header'}>
